bf1c051edf500cdf0f0276f740d74e3d4126baca
Anamaria Cioanca <anamaria.cioanca.rus@gmail.com>
2023-07-20 12:57:20 +0300

    Add automatic code reload in Docker

--- italiandelights/inventory/models.py
+++ italiandelights/inventory/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    quantity = models.FloatField(default=0)
+    unit = models.CharField(max_length=10)
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=75)
+    price = models.FloatField(help_text="Enter the menu price")
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(help_text="Enter the amount of ingredient required to create the menu item")      
+
+class Order(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
