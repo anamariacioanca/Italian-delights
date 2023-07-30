a46ce3f380431d8686fb7022e32fd798a4ddc7d4
Anamaria Cioanca <anamaria.cioanca.rus@gmail.com>
2023-07-30 12:24:07 +0300

    Remove Recipe

--- italiandelights/inventory/admin.py
+++ italiandelights/inventory/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Ingredient, MenuItem, Recipe, Order
+from .models import Ingredient, MenuItem, Order
 
 admin.site.register(Ingredient)
 admin.site.register(MenuItem)
-admin.site.register(Recipe)
 admin.site.register(Order)
 
