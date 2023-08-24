72d5e0af786ce7f8c0bf79bafcebfa85507d0612
Anamaria Cioanca (Rus) <anamaria.cioanca.rus@gmail.com>
2023-08-24 15:49:35 +0300

    Update models

--- italiandelights/inventory/models.py
+++ italiandelights/inventory/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Sum
 
 # Create your models here.
 
@@ -13,20 +14,42 @@ class Ingredient(models.Model):
 class MenuItem(models.Model):
     title = models.CharField(max_length=75)
     price = models.FloatField(help_text="Enter the menu price")
-    ingredients = models.ManyToManyField(Ingredient, through='MenuItemIngredient')
 
     def __str__(self):
         return f"{self.title}"
-
-class MenuItemIngredient(models.Model):
-    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    quantity = models.FloatField(default=0)
-
-    def __str__(self):
-        return f"{self.menu_item.title} - {self.ingredient.name} - {self.quantity}"
-
     
 class Order(models.Model):
     menu_items = models.ManyToManyField(MenuItem)
     timestamp = models.DateTimeField(auto_now_add=True)
+    total_price = models.FloatField(default=0)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # Save the instance first
+        self.total_price = self.calculate_total_price()  # Calculate and set total price
+        super().save(*args, **kwargs)  # Save again to update total_price
+
+        # Update TotalSales instance
+        total_sales, created = TotalSales.objects.get_or_create(date=self.timestamp.date())
+        total_sales.update_totals()
+
+    def calculate_total_price(self):
+        return sum(menu_item.price for menu_item in self.menu_items.all())
+
+class TotalSales(models.Model):
+    date = models.DateField(unique=True)
+    total_order_price = models.FloatField(default=0)
+    total_all_orders_price = models.FloatField(default=0)
+
+    def __str__(self):
+        return f"Total Sales for {self.date}"
+
+    def update_totals(self):
+        orders_on_date = Order.objects.filter(timestamp__date=self.date)
+        self.total_order_price = sum(order.total_price for order in orders_on_date)
+        self.total_all_orders_price = TotalSales.objects.aggregate(Sum('total_order_price'))['total_order_price__sum'] or 0
+        self.save()
+
+
+
+       
+
