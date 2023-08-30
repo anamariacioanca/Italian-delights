65d8e6d38fec672cf6ece308097cd5f1d44d2fdf
Anamaria Cioanca (Rus) <anamaria.cioanca.rus@gmail.com>
2023-08-30 11:40:56 +0300

    Add TotalSales to admin

--- italiandelights/inventory/admin.py
+++ italiandelights/inventory/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Ingredient, MenuItem, Order
+from .models import Ingredient, MenuItem, Order, TotalSales
 
 admin.site.register(Ingredient)
 admin.site.register(MenuItem)
 admin.site.register(Order)
+admin.site.register(TotalSales)
 
