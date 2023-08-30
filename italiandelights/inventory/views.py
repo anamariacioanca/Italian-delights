eccf1ead5fa4ff4b0e6a568ec7aef5b3c0b7f848
Anamaria Cioanca (Rus) <anamaria.cioanca.rus@gmail.com>
2023-08-30 11:25:39 +0300

    Add TotalSales to views and login_required

--- italiandelights/inventory/views.py
+++ italiandelights/inventory/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse_lazy
-from .forms import LoginForm, IngredientForm, MenuItemForm
+from .forms import LoginForm, IngredientForm, MenuItemForm, OrderForm
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Ingredient, MenuItem, Order
+from .models import Ingredient, MenuItem, Order, TotalSales
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
 
@@ -12,19 +15,25 @@ class IngredientList(ListView):
   model = Ingredient
   template_name = "inventory/ingredients.html"
 
-class IngredientCreate(CreateView):
+@method_decorator(login_required, name='dispatch')
+class IngredientCreate(PermissionRequiredMixin, CreateView):
+  permission_required = 'inventory.add_ingredient'
   model = Ingredient
   template_name = "inventory/ingredient_create_form.html"
   form_class = IngredientForm
   success_url = reverse_lazy("ingredients")
 
-class IngredientUpdate(UpdateView):
+@method_decorator(login_required, name='dispatch')
+class IngredientUpdate(PermissionRequiredMixin, UpdateView):
+  permission_required = 'inventory.edit_ingredient'
   model = Ingredient
   template_name = "inventory/ingredient_update_form.html"  
   fields = ["quantity"]  
   success_url = reverse_lazy("ingredients")
 
-class IngredientDelete(DeleteView):
+@method_decorator(login_required, name='dispatch')
+class IngredientDelete(PermissionRequiredMixin, DeleteView):
+  permission_required = 'inventory.delete_ingredient'
   model = Ingredient
   template_name = "inventory/ingredient_delete_form.html"  
   success_url = reverse_lazy("ingredients")
@@ -32,44 +41,29 @@ class IngredientDelete(DeleteView):
 class MenuItemList(ListView):
   model = MenuItem
   template_name = "inventory/menuitem.html"
- 
 
-class MenuItemCreate(CreateView):
-    model = MenuItem
-    template_name = "inventory/menuitem_create_form.html"
-    form_class = MenuItemForm
-    success_url = reverse_lazy("menuitems")
-   
-class MenuItemUpdate(UpdateView):
-    model = MenuItem
-    template_name = "inventory/menuitem_update_form.html"
-    form_class = MenuItemForm
-    success_url = reverse_lazy("menuitems")
+@method_decorator(login_required, name='dispatch')
+class MenuItemCreate(PermissionRequiredMixin, CreateView):
+  permission_required = 'inventory.add_menuitem'
+  model = MenuItem
+  template_name = "inventory/menuitem_create_form.html"
+  form_class = MenuItemForm
+  success_url = reverse_lazy("menuitems")
 
+@method_decorator(login_required, name='dispatch')   
+class MenuItemUpdate(PermissionRequiredMixin, UpdateView):
+  permission_required = 'inventory.edit_menuitem'
+  model = MenuItem
+  template_name = "inventory/menuitem_update_form.html"
+  form_class = MenuItemForm
+  success_url = reverse_lazy("menuitems")
 
-class MenuItemDelete(DeleteView):
+@method_decorator(login_required, name='dispatch')
+class MenuItemDelete(PermissionRequiredMixin, DeleteView):
+  permission_required = 'inventory.delete_menuitem'
   model = MenuItem
   template_name = "inventory/menuitem_delete_form.html"  
   success_url = reverse_lazy("menuitems")
-
-
-# class RecipeList(ListView):
-#     model = Recipe
-#     template_name = "inventory/recipes.html"
-
-# class RecipeCreate(CreateView):
-#     model = Recipe
-#     template_name = "inventory/recipe_create_form.html"
-#     fields = ["menu_item", "ingredient"]
-
-# class RecipeUpdate(UpdateView):
-#     model = Recipe
-#     template_name = "inventory/recipe_update_form.html"
-
-# class RecipeDelete(DeleteView):
-#     model = Recipe
-#     template_name = "inventory/recipe_delete_form.html"
-   
 
 class OrderList(ListView):
   model = Order
@@ -78,20 +72,41 @@ class OrderList(ListView):
 class OrderCreate(CreateView):
   model = Order
   template_name = "inventory/order_create_form.html"
-  fields = ["menu_items", "timestamp"]  
+  fields = ["menu_items"]
+  success_url = reverse_lazy("orders")
+
+  def form_valid(self, form):
+        order = form.save(commit=False)  
+        order.save() 
+        
+        selected_menu_items_ids = self.request.POST.getlist('menu_items')
+        selected_menu_items = MenuItem.objects.filter(pk__in=selected_menu_items_ids)
+        order.menu_items.set(selected_menu_items)  
+        
+        order.total_price = order.calculate_total_price() 
+        order.save()  
+        
+        return super().form_valid(form)
 
 class OrderUpdate(UpdateView):
   model = Order
   template_name = "inventory/order_update_form.html"  
+  fields = ["menu_items"]  
+  success_url = reverse_lazy("orders")
 
 class OrderDelete(DeleteView):
   model = Order
   template_name = "inventory/order_delete_form.html"  
+  success_url = reverse_lazy("orders")
 
-def total_sales_view(request):
-    total_sales = Order.objects.aggregate(Sum('menu_items__price'))['menu_items__price__sum']
-    return render(request, 'inventory/total_sales.html', {'total_sales': total_sales})
-
+@method_decorator(login_required, name='dispatch')
+class TotalSalesList(PermissionRequiredMixin, ListView):
+  permission_required = 'inventory.view_totalsales'
+  model = TotalSales
+  template_name = "inventory/total_sales.html"
+    
+  def get_queryset(self):
+      return TotalSales.objects.order_by("-date")
 
 def login_view(request):
     if request.method == "POST":
@@ -118,5 +133,6 @@ def logout_view(request):
     logout(request)
     return redirect("/")
 
+@login_required(login_url='login')
 def main_view(request):
     return render(request, "inventory/base.html", {})
